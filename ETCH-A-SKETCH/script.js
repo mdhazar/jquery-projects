7ee02dfe6614d6323bab02edd4f1dcb3a8f6aae5
mdhazar <m.derya.hazar@gmail.com>
2025-03-19 00:36:01 +0300

    complete etch a sketch

--- ETCH-A-SKETCH/script.js
+++ ETCH-A-SKETCH/script.js
@@ -1,10 +1,18 @@
 $(document).ready(function () {
   const appendLocation = "main";
+  let isDrawing = false;
+
   $("body").css({
     backgroundColor: "lightgray",
+    minHeight: "100vh",
     margin: 0,
     padding: 0,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
   });
+
   $(appendLocation).css({
     display: "grid",
     gridTemplateColumns: "repeat(16, 1fr)",
@@ -13,17 +21,40 @@ $(document).ready(function () {
     width: "700px",
     height: "700px",
   });
+
   function createGrid(size) {
     $(appendLocation).empty();
     $(appendLocation).css({
       gridTemplateColumns: `repeat(${size}, 1fr)`,
       gridTemplateRows: `repeat(${size}, 1fr)`,
     });
+
     for (let i = 0; i < size ** 2; i++) {
       $(appendLocation).append(
         '<div class="cell" style="background-color: white; border: 1px solid black;"></div>'
       );
     }
+
+    $(".cell")
+      .mousedown(function () {
+        isDrawing = true;
+        $(this).css("background-color", "black");
+      })
+      .mouseover(function () {
+        if (isDrawing) {
+          $(this).css("background-color", "black");
+        }
+      });
+
+    $(document).mouseup(function () {
+      isDrawing = false;
+    });
   }
+  $(appendLocation).after("<button id='clear'>Clear Grid</button>");
+  $(appendLocation).before("<div class='title'>Etch-A-Sketch</div>");
+  $("#clear").click(function () {
+    $(".cell").css("background-color", "white");
+  });
+
   createGrid(16);
 });
